de7dc2de48c0af426bae9899ffe1f851745ad855
yo-aka-gene <yujiokano@keio.jp>
2022-11-21 17:02:13 +0900

    :sparkles: add Generator class

--- sparcity/core/__init__.py
+++ sparcity/core/__init__.py
@@ -1,7 +1,9 @@
 from ._area import subarea
 from ._coord import Coord
+from ._generator import Generator
 
 __all__ = [
     "Coord",
+    "Generator",
     "subarea"
 ]
---
+++ sparcity/core/_generator.py
@@ -0,0 +1,71 @@
+"""
+Abstract class for generator
+"""
+from abc import abstractmethod
+
+import numpy as np
+
+from ._coord import Coord
+
+
+class Generator(Coord):
+    """
+    Abstract class for generator
+
+    Methods
+    -------
+    __init__(self, **kwargs) -> None:
+        initialize attributes listed below.
+
+    __call__() -> Dict[str, np.ndarray]:
+        returns {'X': self.x_2d, 'Y': self.y_2d, 'Z': self.z}
+        The return value is designed for surfaceplot of matplotlib.
+
+    Attributes
+    ----------
+    x: np.ndarray
+        x coordinates (longitude) for the designated area.
+        Fixed to `np.linspace(0, 1000, 10000)`
+
+    y: np.ndarray
+        y coordinates (latitude) for the designated area.
+        Fixed to `np.linspace(0, 1000, 10000)`
+
+    z: np.ndarray
+        z values (e.g., altitude) for the designated area.
+        Generated from given functions.
+
+    x_2d: np.ndarray
+        x coordinates (longitude) corresponding to z
+
+    y_2d: np.ndarray
+        y coordinates (latitude) corresponding to z
+
+    shape: List[int]
+        shape of z
+    """
+    @abstractmethod
+    def __init__(self, **kwargs) -> None:
+        """
+        Parameters
+        ----------
+        kwargs: Any
+            keyword arguments to control the function of z
+        """
+        # self.x and self.y should be fixed.
+        self.x = np.linspace(0, 1000, 10000)
+        self.y = np.linspace(0, 1000, 10000)
+        # actual implementation of self.z is flexible
+        self.z = np.zeros(10000, 10000)
+        self.x_2d, self.y_2d = np.meshgrid(self.x, self.y)
+        self.shape = self.z.shape
+
+    @abstractmethod
+    def __call__(self):
+        """
+        Returns
+        -------
+        CoordinateDict: Dict[str, np.ndarray]
+            {'X': self.x_2d, 'Y': self.y_2d, 'Z': self.z}
+        """
+        return dict(X=self.x, Y=self.y, Z=self.z)
