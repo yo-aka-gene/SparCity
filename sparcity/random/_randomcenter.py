15bae915dedaeaf952484772614d531b610a83b0
yo-aka-gene <yujiokano@keio.jp>
2022-11-23 01:42:24 +0900

    :sparkles: add random_coord

---
+++ sparcity/random/_randomcenter.py
@@ -0,0 +1,44 @@
+"""
+function for random choice of the center coordination
+"""
+from typing import Tuple
+
+import numpy as np
+
+from sparcity import Coord
+from sparcity.dev import typechecker
+from ._random import seedfixer
+
+
+def random_coord(
+    field: Coord,
+    seed: int
+) -> Tuple[float]:
+    """
+    random choice of (x, y) coordinate
+
+    Parameters
+    ----------
+    field: Coord
+        original field to choose the random coordinate from
+
+    seed: int
+        random seed
+
+    Returns
+    -------
+    (x, y): Tuple[float]
+        tuple of floats (length == 2)
+    """
+    typechecker(field, Coord, "field")
+    typechecker(seed, int, "seed")
+    seedfixer(seed, 2, 0)
+    x = (
+        field.x.max() - field.x.min()
+    ) * np.random.random() + field.x.min()
+    seedfixer(seed, 2, 1)
+    y = (
+        field.y.max() - field.y.min()
+    ) * np.random.random() + field.y.min()
+
+    return (x, y)
