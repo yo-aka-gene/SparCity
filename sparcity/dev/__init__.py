e828540340d86270e97438739bfb7d295237bb46
yo-aka-gene <yujiokano@keio.jp>
2022-11-26 02:47:16 +0900

    :sparkles::recycle: add is_monotonic_increase and refine implementation in is_subarray

--- sparcity/dev/__init__.py
+++ sparcity/dev/__init__.py
@@ -1,7 +1,8 @@
 from ._checker import typechecker, valchecker
-from ._numpy_extensions import is_subarray
+from ._numpy_extensions import is_subarray, is_monotonical_increasing
 
 __all__ = [
+    "is_monotonical_increasing",
     "is_subarray",
     "typechecker",
     "valchecker"
