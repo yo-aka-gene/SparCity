f52f2c20a33d63012c0f096d9a05c3abc815ec72
yo-aka-gene <yujiokano@keio.jp>
2022-11-28 01:40:27 +0900

    :bug: fix bug in predict

--- sparcity/gaussian_process/_kissgp.py
+++ sparcity/gaussian_process/_kissgp.py
@@ -236,7 +236,7 @@ class KISSGPRegression(gpt.models.ExactGP):
         with t.no_grad(), gpt.settings.fast_pred_var():
             predicted = self.likelihood(
                 self(testdata.as_tensor()[0])
-            ).mean.view(*testdata.shape)
+            ).mean.view(*testdata.field.shape)
 
         return Coord(
             x=testdata.x,
